98d2da1b0022a1e256d824e732c87c68b9bcd585
Joseph Petersen <josephp90@gmail.com>
2022-01-13 04:43:48 +0100

    add husky config

--- dist/index.js
+++ dist/index.js
@@ -78499,7 +78499,7 @@ module.exports = VerifyStream;
 const path = __nccwpck_require__(71017);
 const fs = __nccwpck_require__(18962);
 const stripBom = __nccwpck_require__(88551);
-const parseJson = __nccwpck_require__(81680);
+const parseJson = __nccwpck_require__(86615);
 const pify = __nccwpck_require__(64810);
 
 const parse = (data, filePath, options = {}) => {
@@ -79291,47 +79291,6 @@ function patch (fs) {
     return false
   }
 }
-
-
-/***/ }),
-
-/***/ 81680:
-/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
-
-"use strict";
-
-const errorEx = __nccwpck_require__(23505);
-const fallback = __nccwpck_require__(55586);
-
-const JSONError = errorEx('JSONError', {
-	fileName: errorEx.append('in %s')
-});
-
-module.exports = (input, reviver, filename) => {
-	if (typeof reviver === 'string') {
-		filename = reviver;
-		reviver = null;
-	}
-
-	try {
-		try {
-			return JSON.parse(input, reviver);
-		} catch (err) {
-			fallback(input, reviver);
-
-			throw err;
-		}
-	} catch (err) {
-		err.message = err.message.replace(/\n/g, '');
-
-		const jsonErr = new JSONError(err);
-		if (filename) {
-			jsonErr.fileName = filename;
-		}
-
-		throw jsonErr;
-	}
-};
 
 
 /***/ }),
@@ -102805,6 +102764,47 @@ const pTry = (fn, ...arguments_) => new Promise(resolve => {
 
 module.exports = pTry;
 module.exports["default"] = pTry;
+
+
+/***/ }),
+
+/***/ 86615:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+"use strict";
+
+const errorEx = __nccwpck_require__(23505);
+const fallback = __nccwpck_require__(55586);
+
+const JSONError = errorEx('JSONError', {
+	fileName: errorEx.append('in %s')
+});
+
+module.exports = (input, reviver, filename) => {
+	if (typeof reviver === 'string') {
+		filename = reviver;
+		reviver = null;
+	}
+
+	try {
+		try {
+			return JSON.parse(input, reviver);
+		} catch (err) {
+			fallback(input, reviver);
+
+			throw err;
+		}
+	} catch (err) {
+		err.message = err.message.replace(/\n/g, '');
+
+		const jsonErr = new JSONError(err);
+		if (filename) {
+			jsonErr.fileName = filename;
+		}
+
+		throw jsonErr;
+	}
+};
 
 
 /***/ }),
